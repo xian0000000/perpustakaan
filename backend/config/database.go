bbb4bb33f707f2d5944913f38905f860debaf78d
xian0000000 <emmachan00000@gmail.com>
2026-03-12 08:09:35 +0700

    update

--- backend/config/database.go
+++ backend/config/database.go
@@ -2,10 +2,10 @@ package config
 
 import (
 	"backend/models"
-	"fmt"
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -17,25 +17,27 @@ var PEMINJAMAN []models.Peminjaman
 
 func ConnectDB() {
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+	// load .env kalau jalan di local
+	godotenv.Load()
+
+	dsn := os.Getenv("DATABASE_URL")
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("gagal connect database", err)
+		log.Fatal("gagal connect database:", err)
 	}
 
 	DB = db
+
 	DB.AutoMigrate(
 		&models.User{},
 		&models.Buku{},
 		&models.Peminjaman{},
 	)
+
 	DB.Find(&USER)
 	DB.Find(&BUKU)
 	DB.Find(&PEMINJAMAN)
+
 	log.Println("berhasil connect database")
 }
